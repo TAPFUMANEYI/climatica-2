cfb80a5936cb3a121e35ec0c63614a293320f6e2
Kimberly Tapfumaneyi <kimberlytapfumaneyi16@gmail.com>
2024-07-18 00:41:54 +0200

    tilelayer

--- src/Components/map.js
+++ src/Components/map.js
@@ -6,3 +6,14 @@ import 'leaflet/dist/leaflet.css'; // Leaflet's CSS for proper map styling
 const Map = ({ coords }) => {
   // If coords are not provided, do not render the map
   if (!coords) return null;
+
+  return (
+    // MapContainer sets up the map with initial center and zoom level
+    <MapContainer center={coords} zoom={13} style={{ height: '400px', width: '100%' }}>
+
+      {/* TileLayer specifies the map tiles and attribution */}
+      <TileLayer
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" // URL for OpenStreetMap tiles
+        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors' 
+      />
+      
