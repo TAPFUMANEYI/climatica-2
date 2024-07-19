260c2979f863f544dbd563f4534a1d34b35d0352
Kimberly Tapfumaneyi <kimberlytapfumaneyi16@gmail.com>
2024-07-19 09:31:25 +0200

    populating weather object

--- src/App.js
+++ src/App.js
@@ -20,4 +20,14 @@ function App() {
 	  let MT = Math.round(data.main.temp);
 	  let FL = Math.round(data.main.feels_like);
 	
-  
+	  const weather = {
+		location: `Weather in ${data.name}`,
+		temperature: `Temperature: ${MT} C`,
+		feelsLike: `Feels Like: ${FL} C`,
+		humidity: `Humidity: ${data.main.humidity} %`,
+		wind: `Wind: ${data.wind.speed} km/h`,
+		condition: `Weather Condition: ${data.weather[0].description}`,
+	};
+
+	setWeatherInfo(weather);
+})
